8a022f98d142da72f1678e530641a89e03af52fb
Sean Kelly <shonkelli@gmail.com>
2019-02-05 02:57:12 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -61,7 +61,7 @@ def organize_schools(hash)
   
   cities = hash.values
   cities.map {|x| cities_array << x[:location]}
-  cities_array!
+  cities_array.uniq!
   
   
   cities_array.each do |city|
